c4613a0db70e335a23149e651d596e20d390364a
andy12345638 <andy12345638@gmail.com>
2018-04-28 22:59:22 +0800

    增加jquery讓行口選擇階層化

--- order_list_form.php
+++ order_list_form.php
@@ -2,10 +2,102 @@
 
 
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<link rel="stylesheet" href="udform.css">
-</head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>新增訂單</title>
+	<link rel="stylesheet" href="udform.css">
+	<script src="http://127.0.0.1/jquery.min.js"></script>
+
+        <script>
+            $(document).ready(function(){
+<?php
+function getconsignee($station){
+	include("mysql_connect.inc.php");
+	$sql="select * from consignee_list where station ='".$station."'";
+	if ($result=mysqli_query($con, $sql)) {
+		#echo "Get List successful!";
+	} else {
+		echo "Error Getting List " . mysqli_error($con);
+	}
+	mysqli_close($con);
+	return $result;
+};
+
+$sql="SELECT station FROM fruit_database.consignee_list group by station;";#選出所有地點
+include("mysql_connect.inc.php");
+if ($station_sql=mysqli_query($con, $sql)) {
+	} else {
+		echo "Error Getting List " . mysqli_error($con);
+}
+$station_array=array();
+$stations="'";
+while($row = $station_sql->fetch_array())
+	{
+		#echo $row['station'];
+		array_push($station_array,$row['station']);
+		$stations=$stations."','".$row['station'];
+	} 
+#print_r( $station_array);
+
+$stations=$stations."'";
+$stations=substr($stations,3,strlen($stations)-1);
+#echo $stations;
+#echo $station_array[0];
+$consignees_id=array();
+for ($i=0;$i<count($station_array);$i++){
+	$consignee_sql=getconsignee($station_array[$i]);
+	#$consignee_array=array();
+	#print_r($consignee_sql);
+	#$consignees=array();
+	$consignees[$i]="'";
+	$consignees_id[$i]="'";
+	while($row = $consignee_sql->fetch_array())
+		{
+			#echo $row['station'];
+			#array_push($consignee_array,$row['consignee']);
+			$consignees[$i]=$consignees[$i]."','".$row['consignee'];
+			$consignees_id[$i]=$consignees_id[$i]."','".$row['consignee_id'];
+		} 
+	$consignees[$i]=$consignees[$i]."'";
+	$consignees[$i]=substr($consignees[$i],3,strlen($consignees[$i])-1);
+	$consignees_id[$i]=$consignees_id[$i]."'";
+	$consignees_id[$i]=substr($consignees_id[$i],3,strlen($consignees_id[$i])-1);
+	#echo $consignees[$i];
+	#echo $consignees_id[$i];
+}
+
+	echo "stations=[".$stations."];";
+
+?>
+                var inner="";
+                for(var i=0;i<stations.length;i++){
+                    inner=inner+'<option value=i>'+stations[i]+'</option>';
+                }
+                $("#stations-list").html(inner)
+                
+                var consignees=new Array();
+                var consignees_id=new Array();
+<?php
+
+	for ($j=0;$j <count($station_array);$j++){
+		echo  "consignees[".$j."]=[".$consignees[$j]."];\n";
+		echo  "consignees_id[".$j."]=[".$consignees_id[$j]."];\n";
+	}
+?>
+
+                $("#stations-list").change(function(){
+                    index=this.selectedIndex;
+                    var Sinner="";
+                    for(var i=0;i<consignees[index].length;i++){
+                        Sinner=Sinner+'<option value='+consignees_id[index][i]+'>'+consignees[index][i]+'</option>';
+                    }
+                    $("#cosignee-list").html(Sinner);
+                });
+                $("#stations-list").change();
+            });
+        </script>
+
+	</head>
 
 <body>
 <grid><div class="col-md-1-2">
@@ -29,6 +121,7 @@ function getlist($list){
 <!--
 #html開始
 -->
+<h1>訂單</h1>
 
 <form action="order_list_receive.php" method="post">
 <table>
@@ -95,7 +188,7 @@ echo "<td>	  <input type=\"date\" name=\"date\" value=\"".$today."\">";
 	<td >	<input type="text" name="trucking_money" maxlength="10" size="10" required><br>
 	</td>
 </tr>  
-
+<!--
 <tr>
 	<td >收貨人編號 consignee_id</td> 
 	<td >
@@ -109,7 +202,20 @@ echo "<td>	  <input type=\"date\" name=\"date\" value=\"".$today."\">";
 		?>
 		</select>	
 	</td>
-</tr>  
+</tr> -->  
+
+<tr>
+	<td >行口地點 station</td> 
+	<td >
+		<select id="stations-list"></select>	
+	</td>
+</tr>
+<tr>
+	<td >行口名稱 consignee_id</td> 
+	<td >
+		<select name="consignee_id" id="cosignee-list"></select>	
+	</td>
+</tr>
 
 <tr>
 	<td >備註 note</td> 
