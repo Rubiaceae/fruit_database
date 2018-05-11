f565038ba6c90609c60c15838117e77712d78335
andy12345638 <andy12345638@gmail.com>
2018-05-11 09:18:12 +0800

    調整成form自我接收post，最後再丟給receive

--- settle_form.php
+++ settle_form.php
@@ -4,7 +4,48 @@
 
 <?php
 session_start();
+function remove_empty($array) {
+  return array_values(array_unique(array_filter($array, 'strlen')));
+}
 
+function addDayswithdate($date,$days){
+
+    $date = strtotime("+".$days." days", strtotime($date));
+    return  date("Y-m-d", $date);
+
+}
+
+function barcode_to_sql($barcode){
+	$date='20'.substr($barcode,1,2).'-'.substr($barcode,3,2).'-'.substr($barcode,5,2);
+	$days=substr($barcode,7,2);
+	$enddate=addDayswithdate($date,$days);
+	$id=substr($barcode,9,4);
+	switch(strtoupper(substr($barcode,0,1))){
+	case 'O':
+		$sql=	'SELECT * FROM fruit_database.order_list
+			left join fruit_database.trucking_list  on trucking_list.trucking_id=order_list.trucking_id 
+			left join fruit_database.consignee_list  on consignee_list.consignee_id=order_list.consignee_id
+			left join fruit_database.driver_list  on driver_list.driver_id=order_list.driver_id
+			where order_list.driver_id=\''.$id.'\'
+			and order_list.date between \''.$date.'\' and \''.$enddate.'\'
+			order by order_list.order_id
+			;';
+		break;
+
+	case 'C':
+		$sql=	'SELECT sum(trucking_money),sum(consignee_money), consignee_list.consignee,consignee_list.station  FROM fruit_database.order_list 
+			left join fruit_database.trucking_list  on trucking_list.trucking_id=order_list.trucking_id 
+			left join fruit_database.consignee_list  on consignee_list.consignee_id=order_list.consignee_id
+			left join fruit_database.driver_list  on driver_list.driver_id=order_list.driver_id
+			where order_list.consignee_id=\''.$id.'\'
+			and order_list.date between \''.$date.'\' and \''.$enddate.'\'
+			and order_list.consignee_money_settle is null
+			group by order_list.consignee_id
+			;';
+		break;
+	}
+	return $sql;
+}
 
 ?>
 <!--
@@ -14,6 +55,7 @@ html開始
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 	<meta name="viewport" content="width=device-width">
+	<link rel="stylesheet" type="text/css" href="theme.css">
 	<title>條碼單號輸入</title>
 	<script src="jquery.min.js"></script> 
 	<script>
@@ -33,18 +75,6 @@ html開始
 </head>
 <body >
 	<script src="/addInput.js" language="Javascript" type="text/javascript"></script>
-<!--	<script language="Javascript" type="text/javascript">
-	$(".inputs").keyup(function () {
-		if (this.value.length == this.maxLength) {
-			var $next = $(this).next('.inputs');
-			if ($next.length)
-				$(this).next('.inputs').focus();
-			else
-				$(this).blur();
-		}
-	});
-	</script>
--->
 	<script>
 	
 	function autotab(current,to){
@@ -60,58 +90,137 @@ html開始
 	document.barcodeform.barcode0.focus()
 	}
 	</script>
-<!--
-	<script>
-	window.onload = function () {	
-		if((document.getElementById('barcode0').value)=="text")
-		{
-			 alert('Hi, I am alert box.');
-		}
-	}
-	</script>
--->
+
 <?php
 
 if(!isset($_SESSION['name']) || empty($_SESSION['name'])){
 	echo "尚未登入!";
 	echo "<script>setTimeout(function(){location.href='login_form.php';},2000);</script><!--五秒後自動回首頁-->";
+}elseif($_POST['token'] == "xAD5l9weDCqKkYgZNd1ICxn4"){
+	$cbarcode=remove_empty($_POST['barcode']);
+	#print_r($cbarcode);
+	$type=strtoupper(substr($cbarcode[0],0,1));
+	#條碼數量	
+	$barlen=count($cbarcode);
+	#echo $type;
+	#所有單號同一類別
+	foreach($cbarcode as $barcode){
+		$newtype=strtoupper(substr($barcode,0,1));
+		if($type != $newtype){
+			echo "有不同型態的單號";
+			exit;		
+		}
+		if(strlen($barcode)!=13){
+			echo "錯誤的條碼長度";
+			exit;	
+		}
+	
+	}
+	#單號類型符合
+	if($type != 'O' && $type != 'T' && $type != 'C' && $type != 'D'){
+		echo '錯誤的條碼型態';
+		exit;
+	}
+
+	#顯示單號類型
+	switch($type){
+	case 'O':
+		echo '司機送貨條碼<br>';
+		break;
+	case 'T':
+		echo '南部代收金條碼<br>';
+		break;
+	case 'C':
+		echo '行口運金條碼<br>';
+		break;
+	case 'D':
+		echo '司機薪資條碼<br>';
+		break;
+	}
+	
+	echo "條碼數量 " .$barlen. " 個</br>";
+	#讀取mysql	
+	include("mysql_connect.inc.php");
+	
+	
+#	if (mysqli_query($con, $sql)) {
+#		echo $station." info updated successfully!</br>";
+#	} else {
+#		echo "Error updating record: " . mysqli_error($con);
+#	}
+#	mysqli_close($con);
+
+	echo "<table id=\"settle_table\" border=1>";
+	switch($type){
+	case 'O':
+		echo '司機送貨條碼<br>';
+		break;
+	case 'T':
+		echo '南部代收金條碼<br>';
+		break;
+	case 'C':
+#		echo '行口運金條碼<br>';
+		echo "<tr><th>條碼</th><th>開始日期</th><th>結束日期</th><th>行口</th><th>市場</th><th>代收金總額</th><th>運金總額</th><th>總額</th></tr>";
+		$i=0;
+		$sum_trucking_money=0;
+		$sum_consignee_money=0;
+		$sum_money=0;
+		foreach($cbarcode as $barcode){
+#			echo $barcode;
+			$date='20'.substr($barcode,1,2).'-'.substr($barcode,3,2).'-'.substr($barcode,5,2);
+			$days=substr($barcode,7,2);
+			$enddate=addDayswithdate($date,$days);
+
+			$sql=barcode_to_sql($barcode);
+#			echo $sql.'<br>';
+			if ($result=mysqli_query($con, $sql)) {
+#				echo " info updated successfully!</br>";
+				#echo num_rows($result);
+							
+			} else {
+				echo "Error sql " . mysqli_error($con);
+			}
+
+			if($result ->num_rows === 0){
+				echo "<tr> <td class='warring'>" .$barcode."</td> <td  class='warring' colspan=\"7\">查無此單號,或是已經輸入過。</td></tr>";
+							
+			}else{
+			
+				while ($row = $result->fetch_array()) {
+		  			echo "<tr> <td>" . $barcode . "</td> <td>" . $date . "</td><td>" . $enddate . "</td><td>" . $row['consignee']."</td> <td>" . $row['station']."</td>  <td>" . $row['sum(trucking_money)']."</td><td>" . $row['sum(consignee_money)']."</td><td>" . strval(floatval($row['sum(consignee_money)'])+floatval($row['sum(trucking_money)']))."</td></tr>\n";
+				$sum_trucking_money=$sum_trucking_money+floatval($row['sum(trucking_money)']);
+				$sum_consignee_money=$sum_consignee_money+floatval($row['sum(consignee_money)']);
+				$sum_money=$sum_money+floatval($row['sum(trucking_money)'])+floatval($row['sum(consignee_money)']);
+				};
+			}
+
+		$i++;
+		}
+		echo "<tr><td></td><td></td><td></td><td></td><td></td><td>".$sum_trucking_money."</td><td>".$sum_consignee_money."</td><td>".$sum_money."</td></tr>";
+		mysqli_close($con);
+		break;
+	case 'D':
+		echo '司機薪資條碼<br>';
+		break;
+	}
+	echo "</table></br>";
+	echo '<input type ="button" onclick="window.location = document.referrer;" value="回到上一頁"></input>';
+
+
 }else{
 
-	echo '<form name=barcodeform action="settle_receive.php" method="post">';
+	echo '<form name=barcodeform action="settle_form.php" method="post">';
 	echo "<h1>輸入條碼單號</h1></br>\n";
 	echo '<input type="button" value="增加欄位"      onClick="addInput(\'dynamicInput\');"></br></br>';
-#	echo "1.<input type=\"text\" name=\"barcode1\" ></br></br>";
-#	echo "<p id=form_list></p></br>\n";
 	echo "<input type=\"hidden\" name=\"token\" value=\"xAD5l9weDCqKkYgZNd1ICxn4\">\n";
-#	echo "<input type=\"buttom\" value=\"增加欄位\" >\n";
-
 	echo '<div id="dynamicInput">';
-#	echo '1. <input class="input" type="text" name="barcode[]" onchange="addInput(\'dynamicInput\')"  data-maxlength="4">';
-#	echo '1. <input class="inputs" type="text" name="barcode[]" onClick="addInput(\'dynamicInput\')"  data-maxlength="4">';	
-#	echo '1. <input class="inputs" type="text" name="barcode[]" data-maxlength="4"></br>';
-#	echo '2. <input class="inputs" type="text" name="barcode[]" data-maxlength="4"></br>';
-#	echo '3. <input class="inputs" type="text" name="barcode[]" data-maxlength="4"></br>';
-#	echo '4. <input class="inputs" type="text" name="barcode[]" data-maxlength="4"></br>';
-#	echo '5. <input class="inputs" type="text" name="barcode[]" data-maxlength="4"></br>';
-#	echo '6. <input class="inputs" type="text" name="barcode[]" data-maxlength="4"></br>';
-#	echo '7. <input class="inputs" type="text" name="barcode[]" data-maxlength="4"></br>';
-#	echo '8. <input class="inputs" type="text" name="barcode[]" data-maxlength="4"></br>';
-#	echo '9. <input class="inputs" type="text" name="barcode[]" data-maxlength="4"></br>';
-#	echo '10. <input class="inputs" type="text" name="barcode[]" data-maxlength="4">';
-
 
 	$i=0;
 	for($i=0 ; $i<10 ; $i++ ){
 	echo strval($i+1).'. <input type="text" id= barcode'.$i.' name="barcode[]" size=13 onKeyup="autotab(this, \'barcode'.strval($i+1).'\')" maxlength=13><br><br>';
 	}
 	echo '</div>';
-	#echo '<input type="text" name="barcode0" size=10 onKeyup="autotab(this, document.barcodeform.barcode1)" maxlength=1><br>';
-	#echo '<input type="text" name="barcode1" size=10 onKeyup="autotab(this, document.barcodeform.barcode2)" maxlength=3><br>' ;
-	#echo '<input type="text" name="barcode2" size=10 onKeyup="autotab(this, document.barcodeform.barcode3)" maxlength=3><br>';
-	#echo '<input type="text" name="barcode3" size=5 maxlength=4>';
-
-
-
+	
 	echo "<input type=\"submit\" value=\"送出條碼\">\n";
 	echo "<input type=\"reset\" value=\"清除表單\">\n";
 	echo '<input type ="button" onclick="javascript:location.href=\'index.html\'" value="回首頁"></input>';
--- settle_receive.php
+++ settle_receive.php
@@ -36,6 +36,17 @@ function barcode_to_sql($barcode){
 			order by order_list.order_id
 			;';
 		break;
+
+	case 'C':
+		$sql=	'SELECT sum(trucking_money),sum(consignee_money), consignee_list.consignee,consignee_list.station  FROM fruit_database.order_list 
+			left join fruit_database.trucking_list  on trucking_list.trucking_id=order_list.trucking_id 
+			left join fruit_database.consignee_list  on consignee_list.consignee_id=order_list.consignee_id
+			left join fruit_database.driver_list  on driver_list.driver_id=order_list.driver_id
+			where order_list.consignee_id=\''.$id.'\'
+			and order_list.date between \''.$date.'\' and \''.$enddate.'\'
+			group by order_list.consignee_id
+			;';
+		break;
 	}
 	return $sql;
 }
@@ -46,6 +57,7 @@ function barcode_to_sql($barcode){
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 	<meta name="viewport" content="width=device-width">
+	<link rel="stylesheet" type="text/css" href="theme.css">
 </head>
 
 
@@ -99,14 +111,68 @@ if( $_POST['token'] == "xAD5l9weDCqKkYgZNd1ICxn4"){
 	echo "條碼數量 " .$barlen. " 個</br>";
 	#讀取mysql	
 	include("mysql_connect.inc.php");
-	$i=0;
-	foreach($cbarcode as $barcode){
-		$sql[$i]=barcode_to_sql($barcode);
-		echo $sql[$i].'<br>';
-		$i++;
-	}
+	
+	
+#	if (mysqli_query($con, $sql)) {
+#		echo $station." info updated successfully!</br>";
+#	} else {
+#		echo "Error updating record: " . mysqli_error($con);
+#	}
+#	mysqli_close($con);
 
-    
+	echo "<table id=\"settle_table\" border=1>";
+	switch($type){
+	case 'O':
+		echo '司機送貨條碼<br>';
+		break;
+	case 'T':
+		echo '南部代收金條碼<br>';
+		break;
+	case 'C':
+#		echo '行口運金條碼<br>';
+		echo "<tr><td>條碼</td><td>開始日期</td><td>結束日期</td><td>行口</td><td>市場</td><td>代收金總額</td><td>運金總額</td><td>總額</td></tr>";
+		$i=0;
+		$sum_trucking_money=0;
+		$sum_consignee_money=0;
+		$sum_money=0;
+		foreach($cbarcode as $barcode){
+#			echo $barcode;
+			$date='20'.substr($barcode,1,2).'-'.substr($barcode,3,2).'-'.substr($barcode,5,2);
+			$days=substr($barcode,7,2);
+			$enddate=addDayswithdate($date,$days);
+
+			$sql=barcode_to_sql($barcode);
+#			echo $sql.'<br>';
+			if ($result=mysqli_query($con, $sql)) {
+#				echo " info updated successfully!</br>";
+				echo num_rows($result);
+				if(num_rows($result)=== 0){
+					echo $barecode."查無此單號,或是已經輸入過。";
+				exit;
+			}
+			} else {
+				echo "Error sql " . mysqli_error($con);
+			}
+
+			
+			while ($row = $result->fetch_array()) {
+	  			echo "<tr> <td>" . $barcode . "</td> <td>" . $date . "</td><td>" . $enddate . "</td><td>" . $row['consignee']."</td> <td>" . $row['station']."</td>  <td>" . $row['sum(trucking_money)']."</td><td>" . $row['sum(consignee_money)']."</td><td>" . strval(floatval($row['sum(consignee_money)'])+floatval($row['sum(trucking_money)']))."</td></tr>\n";
+			$sum_trucking_money=$sum_trucking_money+floatval($row['sum(trucking_money)']);
+			$sum_consignee_money=$sum_consignee_money+floatval($row['sum(consignee_money)']);
+			$sum_money=$sum_money+floatval($row['sum(trucking_money)'])+floatval($row['sum(consignee_money)']);
+			};
+
+		$i++;
+		}
+		echo "<tr><td></td><td></td><td></td><td></td><td></td><td>".$sum_trucking_money."</td><td>".$sum_consignee_money."</td><td>".$sum_money."</td></tr>";
+		mysqli_close($con);
+		break;
+	case 'D':
+		echo '司機薪資條碼<br>';
+		break;
+	}
+	echo "</table>";
+
 }else{
 	echo "ERROR! Wrong token!";
 }
